46b1731863cbbc7dbfd2badeb5afa6bf2ce36fe1
gti <pinchukctac11@gmail.com>
2020-07-14 23:31:21 -0400

    completed the GET requests in the js file

--- server.js
+++ server.js
@@ -9,14 +9,34 @@ const app = express();
 // sets an initial port
 const PORT = process.env.PORT || 3000;
 
+// listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
+
 // sets up the Express app to handle data parsing
 
 
 // getting the html content
 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+  });
+
+app.get("/api/notes", function(req, res) {
+
+    fs.readFile("./db/db.json", "utf-8", function(error, data){
+      if (error) {
+        return console.log(error);
+      } return res.json(JSON.parse(data));
+    });  
+});
 // API POST request
 
 // code for deleting notes
 
-// listener 
+ 
