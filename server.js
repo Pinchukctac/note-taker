63d766659aac7da1a0f43e6040d6c58d5d8aface
gti <pinchukctac11@gmail.com>
2020-07-14 21:12:40 -0400

    adding necessary files, js file has the pseudo code

---
+++ server.js
@@ -0,0 +1,22 @@
+// dependencies 
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+// tells node that we are creating an "express" server
+const app = express();
+
+// sets an initial port
+const PORT = process.env.PORT || 3000;
+
+// sets up the Express app to handle data parsing
+
+
+// getting the html content
+
+
+// API POST request
+
+// code for deleting notes
+
+// listener 
