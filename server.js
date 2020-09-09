466c4278318424a536bdd495a3354d1fa1ae85a5
gti <pinchukctac11@gmail.com>
2020-09-09 18:33:53 -0400

    added post, and delete request, fixed get request

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const PORT = process.env.PORT || 3000;
 // sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public")); 
 
 // getting the html content
-
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
@@ -23,18 +23,45 @@ app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
   });
 
+//api call to read and retrieve all notes as JSON
 app.get("/api/notes", function(req, res) {
 
     fs.readFile("./db/db.json", "utf-8", function(error, data){
-      if (error) {
-        return console.log(error);
-      } return res.json(JSON.parse(data));
+      if (error)  throw error;
+
+       return res.json(JSON.parse(data));
     });  
 });
-// API POST request
-
+// API POST request, Receive a new note, append it to the db.json file, and return the note
+app.post("/api/notes", function(req, res) {
+  let note = (req.body)
+  let id = uuidv4(); 
+  note.id = id
+  let notesArray;
+  let savedNotes = fs.readFileSync("./db/db.json","utf-8");
+  notesArray = JSON.parse(savedNotes);
+  notesArray.push(note);
+  
+  fs.writeFile("./db/db.json", JSON.stringify(notesArray), function(error){
+    if (error) {
+      console.log(error);
+    }
+      console.log(notesArray); 
+      return res.json(notesArray); 
+  });
+});
 // code for deleting notes
+app.delete("/api/notes/:id", function(req, res){
+  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let noteid = req.params.id;
+  console.log(`Deleting note with ID ${noteid}`);
+  savedNotes = savedNotes.filter(currNote => {
+      return currNote.id != noteid;
+    })
 
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    res.json(savedNotes);
+});
  
 
 
